be36e34fd04f23ca709100eb27201ca7b1cac012
AlexanderGhosh <ghoshalexander@gmail.com>
2025-11-11 20:52:28 +0000

    feat(promotions): generalised buy 1 get 1 free test(): (RED) for  buy 1 get 1 free

--- Kata.Checkout.Tests/PromotionTests.cs
+++ Kata.Checkout.Tests/PromotionTests.cs
@@ -1,4 +1,6 @@
-﻿namespace Kata.Checkout.Tests
+﻿using Kata.Checkout.Promotions;
+
+namespace Kata.Checkout.Tests
 {
     [TestFixture]
     internal class PromotionTests
@@ -15,33 +17,53 @@
         }
 
         [Test]
-        public void Apply1_Buy1Get1Free_NoDiscount1AdditionalSKU()
+        public void Apply_1A1A1_Buy1Get1Free_NoDiscount1AdditionalSKU()
         {
+            IPromotion promo = new BuyXGetYFree("A", "A", 1, 1, 1);
+            PromotionResult result = promo.ApplyPromotion(_saleProducts);
+            Assert.IsTrue(result.Applied);
+            Assert.That(result.AppliedSKUs, Is.EquivalentTo(new[] { "A" }));
+            Assert.That(result.AdditionalSKUs, Is.EquivalentTo(new[] { "A" }));
         }
 
         [Test]
-        public void Apply2_Buy1Get1Free_NoDiscount1AdditionalSKU()
+        public void Apply_1A1A2_Buy1Get1Free_NoDiscount1AdditionalSKU()
         {
+            IPromotion promo = new BuyXGetYFree("A", "A", 1, 1, 2);
+            PromotionResult result = promo.ApplyPromotion(_saleProducts);
+            Assert.IsTrue(result.Applied);
+            Assert.That(result.AppliedSKUs, Is.EquivalentTo(new[] { "A" }));
+            Assert.That(result.AdditionalSKUs, Is.EquivalentTo(new[] { "A", "A" }));
         }
 
         [Test]
-        public void Apply1_3MultiBuy_Discount30()
+        public void Apply_1A1B1_Buy1Get1Free_NoDiscount1AdditionalSKU()
         {
+            IPromotion promo = new BuyXGetYFree("A", "B", 1, 1, 1);
+            PromotionResult result = promo.ApplyPromotion(_saleProducts);
+            Assert.IsTrue(result.Applied);
+            Assert.That(result.AppliedSKUs, Is.EquivalentTo(new[] { "A" }));
+            Assert.That(result.AdditionalSKUs, Is.EquivalentTo(new[] { "B" }));
         }
 
-        [Test]
-        public void Apply2_3MultiBuy_Discount30()
-        {
-        }
+        //[Test]
+        //public void Apply1_3MultiBuy_Discount30()
+        //{
+        //}
 
-        [Test]
-        public void Apply1_50PercentOff_Discount10()
-        {
-        }
+        //[Test]
+        //public void Apply2_3MultiBuy_Discount30()
+        //{
+        //}
 
-        [Test]
-        public void Apply2_50PercentOff_Discount10()
-        {
-        }
+        //[Test]
+        //public void Apply1_50PercentOff_Discount10()
+        //{
+        //}
+
+        //[Test]
+        //public void Apply2_50PercentOff_Discount10()
+        //{
+        //}
     }
 }
---
+++ Kata.Checkout/Promotions/BuyXGetYFree.cs
@@ -0,0 +1,26 @@
+﻿namespace Kata.Checkout.Promotions
+{
+    public class BuyXGetYFree(string requiredSKU, string freeSKU, int buyX, int getY, int maxRedemptions) : IPromotion
+    {
+        private readonly string _requiredSKU = requiredSKU;
+        private readonly string _freeSKU = freeSKU;
+        private readonly int _buyX = buyX;
+        private readonly int _getY = getY;
+        private readonly int _maxRedemptions = maxRedemptions;
+
+        public PromotionResult ApplyPromotion(params SaleProduct[] products)
+        {
+            SaleProduct? product = products.FirstOrDefault(p => p.Product.SKU == _requiredSKU);
+            if (product is null) return PromotionResult.Empty;
+
+            PromotionResult res = new();
+            int toApply = product.Quantity / _buyX;
+            toApply = Math.Min(toApply, _maxRedemptions);
+
+            res.AdditionalSKUs.AddRange(Enumerable.Repeat(_freeSKU, toApply * _getY));
+            return res;
+        }
+    }
+
+    public class BuyOneGetOneFree(string sku) : BuyXGetYFree(sku, sku, 1, 1, int.MaxValue);
+}
