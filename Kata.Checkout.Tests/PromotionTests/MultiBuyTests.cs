e0e1f541867761d18fd047ab8a656e1670e5ee65
AlexanderGhosh <ghoshalexander@gmail.com>
2025-11-12 21:00:25 +0000

    test(GREEN): added tests for negative quantities, and unit prices feat(): multi buy can throw argument exceptions

--- Kata.Checkout.Tests/PromotionTests/MultiBuyTests.cs
+++ Kata.Checkout.Tests/PromotionTests/MultiBuyTests.cs
@@ -6,6 +6,49 @@ namespace Kata.Checkout.Tests.PromotionTests
     internal class MultiBuyTests
     {
         [Test]
+        public void Construct_PromoQuantMinus3_PromoPrice130_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(static () => new MultiBuyPromotion(-3, 130));
+        }
+        [Test]
+        public void Apply_PromoQuant3_PromoPrice130_1_Minus1_ThrowsArgumentException()
+        {
+            IPromotion promotion = new MultiBuyPromotion(3, 130);
+            Assert.Throws<ArgumentException>(() => promotion.ApplyPromotion(1, -1));
+        }
+        [Test]
+        public void Construct_PromoQuant3_PromoPriceMinus130_Works()
+        {
+            Assert.DoesNotThrow(() => new MultiBuyPromotion(3, -130));
+        }
+
+        [Test]
+        [TestCase(-50, 0, 0)]
+        [TestCase(-50, 1, -50)]
+        [TestCase(-50, 2, -100)]
+        [TestCase(-50, 3, 130)]
+        [TestCase(-50, 4, 80)]
+        [TestCase(-50, 6, 260)]
+        public void Apply_PromoQuant3_PromoPrice130_NegativeUnitPrice(int x, int y, int z)
+        {
+            IPromotion promotion = new MultiBuyPromotion(3, 130);
+            Assert.That(promotion.ApplyPromotion(x, y), Is.EqualTo(z));
+        }
+
+        [Test]
+        [TestCase(-30, 0, 0)]
+        [TestCase(-30, 1, -30)]
+        [TestCase(-30, 2, 45)]
+        [TestCase(-30, 3, 15)]
+        [TestCase(-30, 4, 90)]
+        [TestCase(-30, 6, 135)]
+        public void Apply_PromoQuant2_PromoPrice45_NegativeUnitPrice(int x, int y, int z)
+        {
+            IPromotion promotion = new MultiBuyPromotion(2, 45);
+            Assert.That(promotion.ApplyPromotion(x, y), Is.EqualTo(z));
+        }
+
+        [Test]
         [TestCase(0, 0, 0)]
         [TestCase(50, 0, 0)]
         [TestCase(50, 1, 50)]
--- Kata.Checkout/Promotions/MultiBuyPromotion.cs
+++ Kata.Checkout/Promotions/MultiBuyPromotion.cs
@@ -1,11 +1,20 @@
 ﻿namespace Kata.Checkout.Promotions
 {
-    public class MultiBuyPromotion(int requiredQuantity, int multiPrice) : IPromotion
+    public class MultiBuyPromotion : IPromotion
     {
-        private readonly int _requiredQuantity = requiredQuantity;
-        private readonly int _multiPrice = multiPrice;
+        private readonly int _requiredQuantity;
+        private readonly int _multiPrice;
+
+        public MultiBuyPromotion(int requiredQuantity, int multiPrice)
+        {
+            if (requiredQuantity < 0) throw new ArgumentException("Required quantity cannot be negative", nameof(requiredQuantity));
+            _requiredQuantity = requiredQuantity;
+            _multiPrice = multiPrice;
+        }
+
         int IPromotion.ApplyPromotion(int unitPrice, int quantity)
         {
+            if (quantity < 0) throw new ArgumentException("Quantity cannot be negative", nameof(quantity));
             int toApply = quantity / _requiredQuantity;
             int notApplied = quantity % _requiredQuantity;
             int multi = _multiPrice * toApply;
