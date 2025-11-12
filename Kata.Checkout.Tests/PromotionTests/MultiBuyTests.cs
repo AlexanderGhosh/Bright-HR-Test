2da842e52d3c21c0720e55a9b0962fd1153a51eb
AlexanderGhosh <ghoshalexander@gmail.com>
2025-11-12 20:30:37 +0000

    test(RED): added new tests for multi buy promotion

---
+++ Kata.Checkout.Tests/PromotionTests/MultiBuyTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Kata.Checkout.Tests.PromotionTests
+{
+    [TestFixture]
+    internal class MultiBuyTests
+    {
+        private IPromotion _promotion;
+        [SetUp]
+        public void Setup()
+        {
+            _promotion = null;
+        }
+
+        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(50, 0, 0)]
+        [TestCase(50, 1, 50)]
+        [TestCase(50, 2, 100)]
+        [TestCase(50, 3, 130)]
+        [TestCase(50, 4, 180)]
+        [TestCase(50, 6, 260)]
+        [TestCase(30, 2, 45)]
+        public void Apply_x_y_ReturnsZ(int x, int y, int z)
+        {
+            Assert.That(_promotion.ApplyPromotion(x, y), Is.EqualTo(z));
+        }
+    }
+}
---
+++ Kata.Checkout/IPromotion.cs
@@ -0,0 +1,7 @@
+﻿namespace Kata.Checkout
+{
+    public interface IPromotion
+    {
+        int ApplyPromotion(int unitPrice, int quantity);
+    }
+}
