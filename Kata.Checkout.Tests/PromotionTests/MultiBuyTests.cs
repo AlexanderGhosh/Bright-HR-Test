bb31a6a63282e26eb5a8e8fd70e3fe28a9dfe8f7
AlexanderGhosh <ghoshalexander@gmail.com>
2025-11-12 21:02:56 +0000

    test(GREEN): tests for negative multi buy price

--- Kata.Checkout.Tests/PromotionTests/MultiBuyTests.cs
+++ Kata.Checkout.Tests/PromotionTests/MultiBuyTests.cs
@@ -53,6 +53,34 @@ namespace Kata.Checkout.Tests.PromotionTests
         [TestCase(50, 0, 0)]
         [TestCase(50, 1, 50)]
         [TestCase(50, 2, 100)]
+        [TestCase(50, 3, -130)]
+        [TestCase(50, 4, -80)]
+        [TestCase(50, 6, -260)]
+        public void Apply_PromoQuant3_PromoPriceNegative130(int x, int y, int z)
+        {
+            IPromotion promotion = new MultiBuyPromotion(3, -130);
+            Assert.That(promotion.ApplyPromotion(x, y), Is.EqualTo(z));
+        }
+
+        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(30, 0, 0)]
+        [TestCase(30, 1, 30)]
+        [TestCase(30, 2, -45)]
+        [TestCase(30, 3, -15)]
+        [TestCase(30, 4, -90)]
+        [TestCase(30, 6, -135)]
+        public void Apply_PromoQuant2_PromoPriceNegative45(int x, int y, int z)
+        {
+            IPromotion promotion = new MultiBuyPromotion(2, -45);
+            Assert.That(promotion.ApplyPromotion(x, y), Is.EqualTo(z));
+        }
+
+        [Test]
+        [TestCase(0, 0, 0)]
+        [TestCase(50, 0, 0)]
+        [TestCase(50, 1, 50)]
+        [TestCase(50, 2, 100)]
         [TestCase(50, 3, 130)]
         [TestCase(50, 4, 180)]
         [TestCase(50, 6, 260)]
