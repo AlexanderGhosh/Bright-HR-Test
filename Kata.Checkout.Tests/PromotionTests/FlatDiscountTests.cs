f1c8c5bb3342ecddca32f0a45c2149b1b7c5b075
AlexanderGhosh <ghoshalexander@gmail.com>
2025-11-14 10:21:25 +0000

    feat(): falt discount promotion test(RED): failing when quantityis negative

---
+++ Kata.Checkout.Tests/PromotionTests/FlatDiscountTests.cs
@@ -0,0 +1,37 @@
+﻿using Kata.Checkout.Promotions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Checkout.Tests.PromotionTests
+{
+    [TestFixture]
+    internal class FlatDiscountTests
+    {
+        [Test]
+        public void Apply_NegativeQuantity_ThrowsArgumentException()
+        {
+            IPromotion promotion = new FlatDiscountPromotion(0);
+            Assert.Throws<ArgumentException>(() => promotion.ApplyPromotion(1, -1));
+        }
+
+        [Test]
+        [TestCase(0, 0, 0, ExpectedResult = 0)]
+        [TestCase(0, 0, 1, ExpectedResult = 0)]
+        [TestCase(0, 1, 1, ExpectedResult = -1)]
+        [TestCase(1, 0, 1, ExpectedResult = 0)]
+        [TestCase(10, 1, 5, ExpectedResult = 5)]
+        [TestCase(10, 2, 5, ExpectedResult = 10)]
+        [TestCase(10, 2, 5, ExpectedResult = 10)]
+        [TestCase(-10, 1, 5, ExpectedResult = -15)]
+        [TestCase(10, 1, -5, ExpectedResult = 15)]
+        public int Apply_UnitPriceX_QuantityY_DiscountZ_ReturnsW(int unitCost, int quantity, int discount)
+        {
+            IPromotion promotion = new FlatDiscountPromotion(discount);
+            int result = promotion.ApplyPromotion(unitCost, quantity);
+            return result;
+        }
+    }
+}
---
+++ Kata.Checkout/Promotions/FlatDiscountPromotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Checkout.Promotions
+{
+    /// <summary>
+    /// Represents a flat discount on a product
+    /// </summary>
+    /// <param name="unitDiscount">The discount to apply per unit</param>
+    public class FlatDiscountPromotion(int unitDiscount) : IPromotion
+    {
+        private readonly int _unitDiscount = unitDiscount;
+        int IPromotion.ApplyPromotion(int unitPrice, int quantity)
+        {
+            return (unitPrice - _unitDiscount) * quantity;
+        }
+    }
+}
