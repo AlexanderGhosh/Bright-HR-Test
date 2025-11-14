157db5769488a0e3cb08b1ed0e61c410512c46e7
AlexanderGhosh <ghoshalexander@gmail.com>
2025-11-14 11:18:21 +0000

    test(RED): buy x get y free tests added feat(): buy x get y free class

---
+++ Kata.Checkout.Tests/PromotionTests/BuyXGetYFree.cs
@@ -0,0 +1,83 @@
+﻿using Kata.Checkout.Promotions;
+
+namespace Kata.Checkout.Tests.PromotionTests
+{
+    [TestFixture]
+    internal class BuyXGetYFree
+    {
+        [Test]
+        public void Constructor_Buy1GetLT1Free_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new BuyXGetYFreePromotion(1, 0));
+            Assert.Throws<ArgumentException>(() => new BuyXGetYFreePromotion(1, -1));
+        }
+        [Test]
+        public void Constructor_BuyLT1Get1Free_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => new BuyXGetYFreePromotion(0, 1));
+            Assert.Throws<ArgumentException>(() => new BuyXGetYFreePromotion(-1, 1));
+        }
+        [Test]
+        public void ApplyPromotion_Buy1Get1Free_NegativeQuantity_ThrowsArgumentException()
+        {
+            IPromotion promotion = new BuyXGetYFreePromotion(1, 1);
+            Assert.Throws<ArgumentException>(() => promotion.ApplyPromotion(0, -1));
+        }
+
+        [Test]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 1, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 0)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(1, 2, ExpectedResult = 1)]
+        [TestCase(1, 3, ExpectedResult = 2)]
+        [TestCase(1, 4, ExpectedResult = 2)]
+        public int ApplyPromotion_Buy1Get1Free_TestCases(int unitPrice, int quantity)
+        {
+            IPromotion promotion = new BuyXGetYFreePromotion(1, 1);
+            return promotion.ApplyPromotion(unitPrice, quantity);
+        }
+
+        [Test]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 1, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 0)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(1, 2, ExpectedResult = 2)]
+        [TestCase(1, 3, ExpectedResult = 1)]
+        [TestCase(1, 4, ExpectedResult = 2)]
+        public int ApplyPromotion_Buy1Get2Free_TestCases(int unitPrice, int quantity)
+        {
+            IPromotion promotion = new BuyXGetYFreePromotion(1, 2);
+            return promotion.ApplyPromotion(unitPrice, quantity);
+        }
+
+        [Test]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 1, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 0)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(1, 2, ExpectedResult = 2)]
+        [TestCase(1, 3, ExpectedResult = 2)]
+        [TestCase(1, 4, ExpectedResult = 3)]
+        public int ApplyPromotion_Buy2Get1Free_TestCases(int unitPrice, int quantity)
+        {
+            IPromotion promotion = new BuyXGetYFreePromotion(2, 1);
+            return promotion.ApplyPromotion(unitPrice, quantity);
+        }
+
+        [Test]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, 1, ExpectedResult = 0)]
+        [TestCase(1, 0, ExpectedResult = 0)]
+        [TestCase(1, 1, ExpectedResult = 1)]
+        [TestCase(1, 2, ExpectedResult = 2)]
+        [TestCase(1, 3, ExpectedResult = 3)]
+        [TestCase(1, 4, ExpectedResult = 2)]
+        public int ApplyPromotion_Buy2Get2Free_TestCases(int unitPrice, int quantity)
+        {
+            IPromotion promotion = new BuyXGetYFreePromotion(2, 2);
+            return promotion.ApplyPromotion(unitPrice, quantity);
+        }
+    }
+}
---
+++ Kata.Checkout/Promotions/BuyXGetYFreePromotion.cs
@@ -0,0 +1,22 @@
+﻿namespace Kata.Checkout.Promotions
+{
+    /// <summary>
+    /// When you want the price for <paramref name="buyX"/> + <paramref name="getY"/> items you get the price for <paramref name="buyX"/>
+    /// </summary>
+    /// <param name="buyX">How many you will pay for</param>
+    /// <param name="getY">How many you will not pay for</param>
+    public class BuyXGetYFreePromotion(int buyX, int getY) : IPromotion
+    {
+        private readonly int _payFor = buyX;
+        private readonly int _getFree = getY;
+        int IPromotion.ApplyPromotion(int unitPrice, int quantity)
+        {
+            return unitPrice * quantity;
+        }
+    }
+
+    /// <summary>
+    /// Buy 1 get 1 free allies for <see cref="BuyXGetYFreePromotion"/>
+    /// </summary>
+    public class BuyOneGetOneFreePromotion() : BuyXGetYFreePromotion(1, 1);
+}
