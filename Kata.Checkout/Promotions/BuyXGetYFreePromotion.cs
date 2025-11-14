fabf6c5c3606d398cabd7616d9702462349021c0
AlexanderGhosh <ghoshalexander@gmail.com>
2025-11-14 11:23:35 +0000

    test(GREEN) feat() finished buy x get y free ApplyPromotion method

--- Kata.Checkout/Promotions/BuyXGetYFreePromotion.cs
+++ Kata.Checkout/Promotions/BuyXGetYFreePromotion.cs
@@ -3,15 +3,35 @@
     /// <summary>
     /// When you want the price for <paramref name="buyX"/> + <paramref name="getY"/> items you get the price for <paramref name="buyX"/>
     /// </summary>
-    /// <param name="buyX">How many you will pay for</param>
-    /// <param name="getY">How many you will not pay for</param>
-    public class BuyXGetYFreePromotion(int buyX, int getY) : IPromotion
+    public class BuyXGetYFreePromotion : IPromotion
     {
-        private readonly int _payFor = buyX;
-        private readonly int _getFree = getY;
+        private readonly int _payFor;
+        private readonly int _getFree;
+
+        /// <summary>
+        /// Initializes an instance of <see cref="BuyXGetYFreePromotion"/> class with the given parameters for this promotion
+        /// </summary>
+        /// <param name="buyX">How many you will pay for</param>
+        /// <param name="getY">How many you will not pay for</param>
+        /// <exception cref="ArgumentException">Thrown when either <paramref name="buyX"/> or <paramref name="getY"/>is < 1></exception>
+        public BuyXGetYFreePromotion(int buyX, int getY)
+        {
+            if (buyX < 1) throw new ArgumentException("buyX must be at least 1", nameof(buyX));
+            if (getY < 1) throw new ArgumentException("getY must be at least 1", nameof(getY));
+            _payFor = buyX;
+            _getFree = getY;
+        }
+
+        /// <inheritdoc/>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="quantity"/> is negative</exception>
         int IPromotion.ApplyPromotion(int unitPrice, int quantity)
         {
-            return unitPrice * quantity;
+            if (quantity < 0) throw new ArgumentException("Quantity cannot be negative", nameof(quantity));
+            int threshold = _payFor + _getFree;
+            int fullSets = quantity / threshold;
+            int remainingItems = quantity % threshold;
+            int total = ((fullSets * _payFor) + remainingItems) * unitPrice;
+            return total;
         }
     }
 
