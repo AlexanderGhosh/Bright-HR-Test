c72934e1ed4001843919c9aa86fc38d985ccd438
AlexanderGhosh <ghoshalexander@gmail.com>
2025-11-14 09:26:44 +0000

    feat(): target both .net8 and .netstandard2

---
+++ Kata.Checkout/Compatibility.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+// This file is used for compatibility with .net standard 2.0
+
+
+#if NETSTANDARD2_0
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal class IsExternalInit{}
+}
+#endif
